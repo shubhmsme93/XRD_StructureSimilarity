fc83e6391831e4f3ea6ec685abd00ccf1878c4ee
Shubham Pandey <67756959+shubhmsme93@users.noreply.github.com>
2021-06-01 19:50:50 -0600

    Update standardize_strc.py

--- standardize_strc.py
+++ standardize_strc.py
@@ -2,36 +2,13 @@
 import spglib
 from pylada.crystal import read, write
 from glob import iglob
-from vladan.format_spglib import *
+from xrd_calculator.format_spglib import *
 import os
 import sys
 from multiprocessing import Pool
 from pylada.vasp import Extract
 #from mpi4py import MPI
 #######################
-'''
-# Initial calls
-comm = MPI.COMM_WORLD
-master = 0
-n_proc = comm.Get_size()
-rank = comm.Get_rank()
-
-##
-def load_balance(n_tasks):
-# Defines the interval each cores needs to compute
-    
-    n_jobs = n_tasks//n_proc
-    balance = n_tasks%n_proc
-
-    if (rank < balance): 
-        i_init = rank*(n_jobs+1)+1
-        i_fin = (rank+1)*(n_jobs+1)
-    else:
-        i_init = balance*(n_jobs+1) + (rank-balance)*(n_jobs)+1
-        i_fin = balance*(n_jobs+1) + (rank-balance+1)*(n_jobs)
-
-    return range(i_init-1, i_fin)
-'''
 
 ########## A function that standardizes structures
 def stdz_strc(name):
@@ -53,20 +30,6 @@ def stdz_strc(name):
             pass
     return
 #######################################
-
-'''
-names=list(iglob('POSCAR_24_atoms_24_*/'))
-
-n_tasks=len(names)
-local_tasks=load_balance(n_tasks)
-
-for ii in local_tasks: #Parallelized loop
-    stdz_strc(names[ii])
-
-
-if (rank == master):
-    print ("DONE !!!")
-'''
 
 # Selecting only those folders with successful VASP
 names=[name for name in iglob('POSCAR_24_atoms_24_*/') if Extract(name).success]
